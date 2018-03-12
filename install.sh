539f2e8497890ff813816c3e82e791f5e59cf579
qingxu.chong <qingxu.chong@yintech.cn>
2018-03-12 18:22:28 +0800

    修改脚本

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ arr=(koa mongoose koa-router node-schedule cheerio superagent superagent-charset
 
 for i in "${!arr[@]}";
 do
-    echo "安装 ${arr[$i]} 成功"
     npm install -s ${arr[$i]} 
     npm install -s @types/${arr[$i]}
+    echo "安装 ${arr[$i]} 成功"
 done
