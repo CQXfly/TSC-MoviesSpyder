70980c78f2ecaf7b8f975a194c9f10e06c62f3c6
CQXfly <905799827@qq.com>
2018-03-13 10:16:22 +0800

    Update connect.sh

--- connect.sh
+++ connect.sh
@@ -5,7 +5,7 @@ spawn sudo ssh root@120.27.93.13
 expect "Password"                 
 send "cqx09143103\r"                
 expect "root@120.27.93.13's password"
-send "CqxAliyun2017"
+send "xxxxxxxx"
 send "sudo -s\r" 
 send "cd /data/logs\r"             
 interact
